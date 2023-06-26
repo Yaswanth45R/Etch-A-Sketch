bb4bba57137e38487bd63c107adfe36cf3631ca5
Yaswanth <pyaswanth4@gmail.com>
2023-06-26 15:08:20 +0530

    Draws the with single color and change the number of squares

---
+++ script.js
@@ -0,0 +1,54 @@
+const mainDiv = document.querySelector('.main-container');
+let mainContainer = document.querySelector('.main-container');
+mainContainer.style.height = "352px";
+mainContainer.style.width = "352px";
+for(let i=0;i<256;i++)
+{
+    let div = document.createElement("div");
+    div.classList.add('grid');
+    mainDiv.appendChild(div);
+};
+
+const divs = document.querySelectorAll('.grid');
+divs.forEach((divd) => divd.addEventListener('mouseenter',colorChange));
+
+function colorChange(event){
+    event.target.style.background='blue'
+}
+
+const clearButton = document.querySelector('.btnClear');
+clearButton.addEventListener('click',resetColor)
+
+function resetColor()
+{
+    divs.forEach((divd)=>(divd.style.background='white'));
+}
+
+const changeBtn = document.querySelector('.btnChangeSquare');
+changeBtn.addEventListener('click',changeSquare);
+
+function changeSquare()
+{
+    let numSqr = prompt('Please enter number of square for each side (must be in range 16-64)');
+    if (numSqr>0 && numSqr<64)
+    {
+        let divs = document.querySelectorAll(".grid");
+        divs.forEach((div)=>{div.remove()});
+
+        for(let i=0;i<numSqr*numSqr;i++)
+        {
+            let div = document.createElement("div");
+            div.classList.add('grid');
+            mainDiv.appendChild(div);
+        };
+
+        mainContainer.style.height = `${((20+2)*(numSqr))}px`;
+        mainContainer.style.width = `${((20+2)*(numSqr))}px`;
+
+        const divds = document.querySelectorAll('.grid');
+        divds.forEach((divd) => divd.addEventListener('mouseenter',colorChange));
+
+        const clearBtn = document.querySelector('.btnClear');
+        clearBtn.addEventListener('click',()=>{divds.forEach((divd)=>(divd.style.background='white'));})
+    }
+}
